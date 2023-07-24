973366fa51cfbe1b16b64f20e6ebb7679f8a274e
khayrat18 <hairatua@yahoo.com>
2023-07-24 10:40:38 +0000

    sorting_algorithms

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+/**
+ * 
+*/
+
+
+
+void insertion_sort_list(listint_t **list)
+{
+    listint_t *current = *list;
+
+
+}
---
+++ 2-selection_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * 
+*/
+
+void selection_sort(int *array, size_t size)
+{
+    size_t i, j, k, min;
+    int temp;
+
+    for (i = 0; i < size - 1; i++)
+    {
+        min = i;
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[min])
+            min = j;
+
+
+        }
+        temp = array[i];
+        array[i] = array[min];
+        array[min] = temp;
+
+        for (k = 0; k < size; k++)
+        {
+            printf("%d", array[k]);
+            if (k < size - 1)
+                printf(", ");
+
+        }
+        printf("\n");
+    }
+
+}
